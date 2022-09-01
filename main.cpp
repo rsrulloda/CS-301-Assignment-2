02db09915e2050f2ce3325c58db9a133f489e3f5
rsrulloda <ronell.rulloda@gmail.com>
2022-09-01 13:33:08 -0700

    Added johnsonTrotter(int n) & printPerm(int n). Implemented main with input handling

--- main.cpp
+++ main.cpp
@@ -4,6 +4,30 @@
 
 using namespace std;
 
+void printPerm(int n) {
+    cout << "test: " << n;
+}
+
+void johnsonTrotter(int n) {
+    int permCount = 0;
+
+    cout << "There are " << permCount << " permutations of the set {" << "}:\n";
+
+    printPerm(n);
+}
+
+
 int main() {
+    int n;
+
+    cout << "Please enter a positive integer between 1 and 25:";
+    cin >> n;
+
+    if(n>=1 && n<=25) {
+        johnsonTrotter(n);
+    } else {
+        cout << "Value entered is not between 1 and 25";
+    }
+
     return 0;
 }
