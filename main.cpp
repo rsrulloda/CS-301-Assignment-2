5cd8fc1cdffc1ec6324ef0f1083a7cadbbfd0b5c
rsrulloda <ronell.rulloda@gmail.com>
2022-09-01 16:39:16 -0700

    Added count() & printRest(). Edited printPerm()

--- main.cpp
+++ main.cpp
@@ -1,17 +1,39 @@
 // Study Group 7 (Alex Milanez, John Edwin Karel Delgado, Ronell Sean Rulloda)
 
 #include <iostream>
-
 using namespace std;
 
+int count(int n) {
+    int count = 1;
+    for (int i=1; i<=n; i++) {
+        count = count * i;
+    }
+    return count;
+}
+
+int printRest(int perm[], bool dir[], int n) {
+    return 0;
+}
+
 void printPerm(int n) {
-    cout << "test: " << n;
+    int perm[n]; // Initialize array for integers of size n
+    bool dir[n]; // Initialize array for directions of size n
+    bool LEFT = true;
+    bool RIGHT = false;
+
+    for(int i=0; i<n; i++) {
+        perm[i] = i + 1; // Stores integers into array perm[n]
+        dir[i] = LEFT; // Sets all integer direction to left
+        cout << perm[i] << " "; // Prints out first perm
+    }
+
+    for(int i=1;i<n;i++) {
+        printRest(perm, dir, n);
+    }
 }
 
 void johnsonTrotter(int n) {
-    int permCount = 0;
-
-    cout << "There are " << permCount << " permutations of the set {" << "}:\n";
+    cout << "There are " << count(n) << " permutations of the set {" << "}:\n";
 
     printPerm(n);
 }
